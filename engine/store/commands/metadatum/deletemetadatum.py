1f360ee449d7079eda6dbc1ca0d9999982cd81c7
Brett Kromkamp <brettkromkamp@gmail.com>
2016-07-13 12:34:30 +0000

    Initial implementation of the DeleteMetadatumCommand class (without testing).

---
+++ engine/store/commands/metadatum/deletemetadatum.py
@@ -0,0 +1,32 @@
+"""
+DeleteMetadatumCommand class. Part of the StoryTechnologies Builder project.
+
+July 13, 2016
+Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
+"""
+
+import sqlite3
+
+from engine.store.topicstoreexception import TopicStoreException
+
+
+class DeleteMetadatumCommand:
+
+    def __init__(self, database_path, identifier=''):
+        self.database_path = database_path
+        self.identifier = identifier
+
+    def do(self):
+        if self.identifier == '':
+            raise TopicStoreException("Missing 'identifier' parameter")
+
+        connection = sqlite3.connect(self.database_path)
+
+        try:
+            with connection:  # https://docs.python.org/3/library/sqlite3.html#using-the-connection-as-a-context-manager
+                connection.execute("DELETE FROM metadatum WHERE identifier = ?", (self.identifier,))
+        except sqlite3.Error as e:
+            raise TopicStoreException(e)
+        finally:
+            if connection:
+                connection.close()
