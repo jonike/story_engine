641e9acd4fcaedf6f25d99f30883d931e4b536dd
Brett Kromkamp <brettkromkamp@gmail.com>
2016-07-13 12:55:08 +0000

    Reverting previous (erroneous) commit.

--- engine/store/commands/metadatum/deletemetadatum.py
+++ engine/store/commands/metadatum/deletemetadatum.py
@@ -1,5 +1,5 @@
 """
-DeleteOccurrenceCommand class. Part of the StoryTechnologies Builder project.
+DeleteMetadatumCommand class. Part of the StoryTechnologies Builder project.
 
 July 13, 2016
 Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
@@ -8,10 +8,9 @@ Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
 import sqlite3
 
 from engine.store.topicstoreexception import TopicStoreException
-from engine.store.commands.metadatum.deletemetadata import DeleteMetadataCommand
 
 
-class DeleteOccurrenceCommand:
+class DeleteMetadatumCommand:
 
     def __init__(self, database_path, identifier=''):
         self.database_path = database_path
@@ -25,8 +24,7 @@ class DeleteOccurrenceCommand:
 
         try:
             with connection:  # https://docs.python.org/3/library/sqlite3.html#using-the-connection-as-a-context-manager
-                connection.execute("DELETE FROM occurrence WHERE identifier = ?", (self.identifier,))
-            DeleteMetadataCommand(self.database_path, self.identifier).do()  # Delete the occurrence's metadata
+                connection.execute("DELETE FROM metadatum WHERE identifier = ?", (self.identifier,))
         except sqlite3.Error as e:
             raise TopicStoreException(e)
         finally:
