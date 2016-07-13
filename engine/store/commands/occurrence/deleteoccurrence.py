eecbe0c924bc8c9ec24b4092b36be0c37bc907e0
Brett Kromkamp <brettkromkamp@gmail.com>
2016-07-13 12:56:48 +0000

    Added the deletion of metadata.

--- engine/store/commands/occurrence/deleteoccurrence.py
+++ engine/store/commands/occurrence/deleteoccurrence.py
@@ -8,6 +8,7 @@ Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
 import sqlite3
 
 from engine.store.topicstoreexception import TopicStoreException
+from engine.store.commands.metadatum.deletemetadata import DeleteMetadataCommand
 
 
 class DeleteOccurrenceCommand:
@@ -25,6 +26,7 @@ class DeleteOccurrenceCommand:
         try:
             with connection:  # https://docs.python.org/3/library/sqlite3.html#using-the-connection-as-a-context-manager
                 connection.execute("DELETE FROM occurrence WHERE identifier = ?", (self.identifier,))
+            DeleteMetadataCommand(self.database_path, self.identifier).do()  # Delete the occurrence's metadata
         except sqlite3.Error as e:
             raise TopicStoreException(e)
         finally:
