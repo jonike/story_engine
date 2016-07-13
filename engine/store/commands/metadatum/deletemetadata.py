9a0d74afb4f52ad3b5fa8b5ac39b016c0518c859
Brett Kromkamp <brettkromkamp@gmail.com>
2016-07-13 12:35:28 +0000

    Initial implementation of the DeleteMetadataCommand class (without testing).

---
+++ engine/store/commands/metadatum/deletemetadata.py
@@ -0,0 +1,32 @@
+"""
+DeleteMetadataCommand class. Part of the StoryTechnologies Builder project.
+
+July 13, 2016
+Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
+"""
+
+import sqlite3
+
+from engine.store.topicstoreexception import TopicStoreException
+
+
+class DeleteMetadataCommand:
+
+    def __init__(self, database_path, entity_identifier=''):
+        self.database_path = database_path
+        self.entity_identifier = entity_identifier
+
+    def do(self):
+        if self.entity_identifier == '':
+            raise TopicStoreException("Missing 'entity identifier' parameter")
+
+        connection = sqlite3.connect(self.database_path)
+
+        try:
+            with connection:  # https://docs.python.org/3/library/sqlite3.html#using-the-connection-as-a-context-manager
+                connection.execute("DELETE FROM metadatum WHERE parent_identifier_fk = ?", (self.entity_identifier))
+        except sqlite3.Error as e:
+            raise TopicStoreException(e)
+        finally:
+            if connection:
+                connection.close()
