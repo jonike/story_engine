57dddbc43c3b5f8a419fb2018525f518ed2b6030
Brett Kromkamp <brett.kromkamp@gmail.com>
2016-08-16 22:30:24 +0200

    Added scene text assets.

--- scripts/map1.py
+++ scripts/map1.py
@@ -20,26 +20,71 @@ repo_path = '/home/brettk/Source/storytechnologies/story-engine/topics.db'
 
 # Define and persist the first (robot) scene.
 asset1 = Asset('scene', 'scene-001.json')
-scene1 = Scene('scene-001', 'Scene 001', 1)
+scene1 = Scene('scene-001', 'Scene One', 1)
 scene1.add_asset(asset1)
+scene1_text = """There can be no thought of finishing for *aiming for the stars*. Both figuratively and literally, it is
+a task to occupy the generations. And no matter how much progress one makes, there is always the thrill of just
+beginning.
+
+Failure is not an option.
+
+We are all connected; To each other, biologically. To the earth, chemically. To the rest of the universe atomically.
+
+What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the
+earth.
+
+Problems look mighty small from 150 miles up.
+"""
+asset2 = Asset('text', data=scene1_text)
+scene1.add_asset(asset2)
 SetSceneCommand(repo_path, scene1).do()
 
 # Define and persist the second (crates) scene.
-asset2 = Asset('scene', 'scene-002.json')
-scene2 = Scene('scene-002', 'Scene 002', 2)
-scene2.add_asset(asset2)
+asset3 = Asset('scene', 'scene-002.json')
+scene2 = Scene('scene-002', 'Scene Two', 2)
+scene2.add_asset(asset3)
+scene2_text = """The sky is the limit only for those who aren't afraid to fly!
+
+Buy why, some say, the moon? Why choose this as our goal? And they may as well ask why climb the highest mountain?
+
+Where ignorance lurks, so too do the frontiers of discovery and imagination.
+"""
+asset4 = Asset('text', data=scene2_text)
+scene2.add_asset(asset4)
 SetSceneCommand(repo_path, scene2).do()
 
 # Define and persist the third (empty) scene.
-asset3 = Asset('scene', 'scene-003.json')
-scene3 = Scene('scene-003', 'Scene 003', 3)
-scene3.add_asset(asset3)
+asset5 = Asset('scene', 'scene-003.json')
+scene3 = Scene('scene-003', 'Scene Three', 3)
+scene3.add_asset(asset5)
+scene3_text = """As I stand out here in the wonders of the unknown at Hadley, I sort of realize there's a fundamental
+truth to our nature, Man must explore ... and this is exploration at its greatest.
+
+The Earth was small, light blue, and so touchingly alone, our home that must be defended like a holy relic. The Earth
+was absolutely round. I believe I never knew what the word round meant until I saw Earth from space.
+
+We want to explore. We're curious people. Look back over history, people have put their lives at stake to go out and
+explore ... We believe in what we're doing. Now it's time to go.
+"""
+asset6 = Asset('text', data=scene3_text)
+scene3.add_asset(asset6)
 SetSceneCommand(repo_path, scene3).do()
 
 # Define and persist the fourth (outside windmill) scene.
-asset4 = Asset('scene', 'scene-004.json')
-scene4 = Scene('scene-004', 'Scene 004', 4)
-scene4.add_asset(asset4)
+asset7 = Asset('scene', 'scene-004.json')
+scene4 = Scene('scene-004', 'Scene Four', 4)
+scene4.add_asset(asset7)
+scene4_text = """When I orbited the Earth in a spaceship, I saw for the first time how beautiful our planet is. Mankind,
+let us preserve and increase this beauty, and not destroy it!
+
+If you could see the earth illuminated when you were in a place as dark as night, it would look to you more splendid
+than the moon.
+
+As I stand out here in the wonders of the unknown at Hadley, I sort of realize there's a fundamental truth to our
+nature, Man must explore ... and this is exploration at its greatest.
+"""
+asset8 = Asset('text', data=scene4_text)
+scene4.add_asset(asset8)
 SetSceneCommand(repo_path, scene4).do()
 
 # Define navigation paths between scenes.
@@ -48,10 +93,10 @@ SetNavigationCommand(repo_path, 'scene-001', 'scene-003', 'east', 'west').do()
 SetNavigationCommand(repo_path, 'scene-003', 'scene-004', 'north', 'south').do()
 
 # Define and persist a character.
-character1 = Character('robot-001', 'Robot 001')
+character1 = Character('robot-001', 'Robot One')
 character1.location = '[3.0, 0.0, 0.0]'  # x ("width"), y ("height"), z ("depth")
-asset4 = Asset('scene', 'robot-001.json')
-character1.add_asset(asset4)
+asset9 = Asset('scene', 'robot-001.json')
+character1.add_asset(asset9)
 character1_text = """## Robot
 
 A **robot** is a mechanical or virtual artificial agent, usually an electromechanical machine that is guided by a computer
@@ -69,19 +114,19 @@ with automated machines that can take the place of humans in dangerous environme
 resemble humans in appearance, behavior, and/or cognition. Many of today's robots are inspired by nature contributing to
 the field of bio-inspired robotics. These robots have also created a newer branch of robotics: soft robotics.
 """
-asset5 = Asset('text', data=character1_text)
-character1.add_asset(asset5)
+asset10 = Asset('text', data=character1_text)
+character1.add_asset(asset10)
 SetCharacterCommand(repo_path, character1, 'scene-001').do()
 
 # Define and persist a prop.
-prop1 = Prop('prop-001', 'Crates 001')
+prop1 = Prop('prop-001', 'Crates One')
 prop1.location = '[2.0, 0.0, 2.0]'  # x ("width"), y ("height"), z ("depth")
-asset6 = Asset('scene', 'crates-001.json')
-prop1.add_asset(asset6)
+asset11 = Asset('scene', 'crates-001.json')
+prop1.add_asset(asset11)
 prop1_text = """## Crate
 
 A **crate** is a large strong container, often made of wood or metal.
 """
-asset7 = Asset('text', data=prop1_text)
-prop1.add_asset(asset7)
+asset12 = Asset('text', data=prop1_text)
+prop1.add_asset(asset12)
 SetPropCommand(repo_path, prop1, 'scene-002').do()
